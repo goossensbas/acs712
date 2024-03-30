d72881b2d91a21bcc6e368048995db28ee3638ca
bas <goossensbas@gmail.com>
2024-03-30 22:24:13 +0100

    added code for OTA updates

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <LiquidCrystal_I2C.h>
 #include "secrets.h"
 #include <IotWebConf.h>
+#include <ArduinoOTA.h>
 
 //#define TAGO
 #define LOCAL
@@ -41,6 +42,24 @@ void handleRoot();
 // #define WASMACHINE_ID 9999
 // #define SENSOR_ID 9999
 // #define SESSION_ID 9999
+// 
+// const char thingName[] = "meter2";
+// const char wifiInitialApPassword[] = "Ucll2024";
+
+// OTA optional settings
+
+// Port defaults to 3232
+// ArduinoOTA.setPort(3232);
+
+// Hostname defaults to esp3232-[MAC]
+// ArduinoOTA.setHostname("myesp32");
+
+// No authentication by default
+// ArduinoOTA.setPassword("admin");
+
+// Password can be set with it's md5 value as well
+// MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+// ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3
 
 ADS1115 ADS(0x48);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -232,12 +251,46 @@ void setup()
   server.on("/", handleRoot);
   server.on("/config", []{ iotWebConf.handleConfig(); });
   server.onNotFound([](){ iotWebConf.handleNotFound(); });
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
 }
 
 void loop()
 {
   while (true)
   {
+    ArduinoOTA.handle();
     iotWebConf.doLoop();
     #ifdef TAGO
     if(!(tago_client.connected())){
